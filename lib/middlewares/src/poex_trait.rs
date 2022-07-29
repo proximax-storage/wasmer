8738a251100bcdd362d4c20fd1887898e4cc9341
changyc9928 <changyc9928@gmail.com>
2022-07-29 10:28:49 +0800

    R21SC-67 https://github.com/proximax-storage/rust-xpx-supercontract-vm/pull/19#discussion_r926369841

--- lib/middlewares/src/poex_trait.rs
+++ lib/middlewares/src/poex_trait.rs
@@ -3,7 +3,8 @@ use wasmer::MiddlewareReaderState;
 use wasmer_types::ModuleInfo;
 
 pub trait PoEx: DynClone {
-    fn add_opcode(&mut self, opcode: u8);
+    fn add_opcode(&mut self, opcode: u8, id: u128);
     fn insert_global(&mut self, module_info: &mut ModuleInfo);
-    fn inject_poex_fn_at_the_end_of_block(&mut self, state: &mut MiddlewareReaderState<'_>);
+    fn inject_poex_fn_at_the_end_of_block(&mut self, state: &mut MiddlewareReaderState<'_>, id: u128);
+    fn add_id(&mut self, id: u128);
 }
